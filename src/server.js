6bcbf71d5cb3e7b24e03dd220101f4cb0d4136d5
cedricahenkorah <cedricahenkorah@gmail.com>
2024-02-27 18:08:06 +0000

    mongo db connection set up

---
+++ src/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const http = require("http");
+const mongoose = require("mongoose");
+const app = require("./app");
+const connectDB = require("./config/dbConnection");
+
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("mongoDB connection error", err);
+});
+
+async function startServer() {
+  await connectDB();
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+startServer();

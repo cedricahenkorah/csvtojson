7898782dd4f3d6531c421a9607fc5445ff8c34e6
cedricahenkorah <cedricahenkorah@gmail.com>
2024-02-28 10:40:59 +0000

    read the json from the db

--- src/csv/csv.controller.js
+++ src/csv/csv.controller.js
@@ -3,17 +3,30 @@ const fs = require("fs");
 const csv = require("csv-parser");
 
 async function csvtojson(req, res) {
-  fs.createReadStream(req.file.path)
-    .pipe(csv())
-    .on("data", async (row) => {
-      const document = new CSV(row);
+  const deleted = await CSV.deleteMany();
 
-      await document.save();
-    })
-    .on("end", () => {
-      console.log("CSV file successfully processed");
-      res.status(200).redirect("/");
-    });
+  if (deleted) {
+    fs.createReadStream(req.file.path)
+      .pipe(csv())
+      .on("data", async (row) => {
+        const document = new CSV(row);
+
+        await document.save();
+      })
+      .on("end", () => {
+        console.log("CSV file successfully processed");
+        res.status(200).redirect("/upload/read");
+      });
+  }
 }
 
-module.exports = { csvtojson };
+async function readJson(req, res) {
+  const json = await CSV.find({}, { __v: 0 }).lean();
+
+  return res.status(200).render("read-json", {
+    path: "/read",
+    json: json,
+  });
+}
+
+module.exports = { csvtojson, readJson };

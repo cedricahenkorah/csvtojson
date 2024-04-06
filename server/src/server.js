21c9a11f800f9e9744bee8c1954839b41b337c00
cedricahenkorah <cedricahenkorah@gmail.com>
2024-04-06 17:26:56 +0000

    removed mongodb connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,22 +1,11 @@
 require("dotenv").config();
 const http = require("http");
-const mongoose = require("mongoose");
 const app = require("./api/app");
-const connectDB = require("./config/dbConnection");
 
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.log("mongoDB connection error", err);
-});
-
 async function startServer() {
-  await connectDB();
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });

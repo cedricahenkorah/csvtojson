048cd57ee8c2cba292ac5836148555d8f2807a6e
cedricahenkorah <cedricahenkorah@gmail.com>
2024-12-26 00:48:40 +0000

    style: add nav links

--- client/app/_components/navbar.tsx
+++ client/app/_components/navbar.tsx
@@ -1,26 +1,35 @@
+import { FileText } from "lucide-react";
 import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <header className="sticky top-0 z-50 bg-white">
-      <nav className="w-full border-b border-b-gray-200">
-        <div className="m-auto lg:max-w-7xl max-w-3xl px-5 lg:px-5 xl:px-0 py-3 lg:py-5 flex justify-between">
-          <div className="flex items-center">
-            <Link href="/">
-              <h1 className="font-bold text-lg">csvtojson</h1>
-            </Link>
-          </div>
-
-          {/* <div className="flex items-center">
-            <div
-              className="inline-flex items-center justify-center px-5 py-2 text-base font-semibold text-black border-2 border-black hover:bg-black hover:text-white transition-all duration-200 focus:bg-black focus:text-white"
-              role="button"
-            >
-              Book Now
-            </div>
-          </div> */}
-        </div>
-      </nav>
+    <header className="border-b">
+      <div className="container flex h-14 items-center">
+        <Link href="/" className="flex items-center space-x-2">
+          <FileText className="h-6 w-6" />
+          <span className="font-bold">csvtojson</span>
+        </Link>
+        <nav className="ml-auto flex gap-4 sm:gap-6">
+          <Link
+            href="#features"
+            className="text-sm font-medium hover:underline underline-offset-4"
+          >
+            Features
+          </Link>
+          <Link
+            href="#examples"
+            className="text-sm font-medium hover:underline underline-offset-4"
+          >
+            Examples
+          </Link>
+          <Link
+            href="https://github.com/cedricahenkorah/csvtojson"
+            className="text-sm font-medium hover:underline underline-offset-4"
+          >
+            GitHub
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 };
